167f160dceb3c4cd4918fced2d4c3789c5354377
Dev Gustavo <113487971+Devgustavogomes@users.noreply.github.com>
2025-09-15 17:49:05 -0300

    Add files via upload

---
+++ BibliotecaMatrizes-main/BibliotecaMatrizes-main/src/Vector.java
@@ -0,0 +1,32 @@
+public class Vector extends Matrix{
+    private int dim;
+    private double[] elements;
+    private boolean isRow = true;
+    // vetor é classe filha de matrix
+    Vector(int dim, double[] elements, boolean isRow){
+        super(isRow ? 1 : dim, isRow ? dim : 1, elements); //construtor de matrix
+        this.isRow = isRow;
+        this.dim = dim;
+        this.elements = elements;
+    }
+
+    public double getElement(int i){
+        return this.elements[i];
+    }
+
+    public void setElement(int i, double value){
+        this.elements[i] = value;
+    }
+
+    public int getDim(){
+        return this.dim;
+    }
+
+    public boolean isRow() {
+        return this.isRow;
+    }
+
+    public void setIsRow(boolean isRow) {
+        this.isRow = isRow;
+    }
+}
